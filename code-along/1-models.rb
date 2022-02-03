af090852acdf0e861e1c8690e230bea7d7d16270
theraabit <mike.raab@kellogg.northwestern.edu>
2022-02-03 02:11:50 +0000

    lab 1

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -12,9 +12,47 @@ Company.destroy_all
 # 1b. check out the model file
 
 # 2. create new companies
+values = {
+    name: "Apple",
+    url: "https://apple.com",
+    city: "Cupertino",
+    state: "CA"
+}
+company = Company.new(values)
+company.save
 
+puts "There are #{Company.all.count} companies"
+
+
+company = Company.new
+company.write_attribute(:name,"Tesla")
+company.write_attribute(:url,"https://tesla.com")
+company.write_attribute(:city,"Palot Alto")
+company.write_attribute(:state,"CA")
+company.save
+
+#puts "There are #{Company.all.count} companies"
 # 3. query companies table
 
+
+#companies = Company.where({ state: "CA"})
+#puts companies.inspect
+
+apple = Company.where({ state: "CA", name: "Apple"})[0]
+#puts apple.inspect
 # 4. read column values from row
+#puts "#{apple.read_attribute(:name)} has a website: #{apple.read_attribute(:url)}"
+puts "#{apple.name} has a website #{apple.url}"
 
 # 5. update attribute value
+#apple.write_attribute( :slogan, "Think Different")
+apple.slogan = "Think Different"
+apple.save
+
+puts apple.inspect
+
+#tesler = Company.new({ name: "Tesler" })
+#tesler.save
+
+#tesler.destroy 
+
--- labs/1-models.rb
+++ labs/1-models.rb
@@ -10,11 +10,27 @@ Contact.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
-
 # 2. create 1-2 new contacts for each company (they can be made up)
-
+values = {
+    first_name: "Tyler",
+    last_name: "Gile",
+    email: "tyler.gile@gmail.com",
+    company_id: "1"
+}
+contacts = Contact.new(values)
+contacts.save
+values = {
+    first_name: "Tim",
+    last_name: "Rzeznik",
+    email: "tim.rzeznik@gmail.com",
+    company_id: "2"
+}
+contacts = Contact.new(values)
+contacts.save
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+puts "There are #{Contact.all.count} contacts"
 
+puts 
 # ---------------------------------
 # Contacts: 4
 # Andy Jassy - andy@amazon.com
